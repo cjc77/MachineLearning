11985e66aeaae76ccc81343eead19af591ecda22
Carson <carsonjamescook@gmail.com>
2019-03-28 13:40:13 -0700

    Finished one more test.

--- MLFromScratch/optimization_tests.py
+++ MLFromScratch/optimization_tests.py
@@ -14,9 +14,24 @@ class QRTestCase(TestCase):
 
     def test_solve_triangular(self):
         for N in range(1, 20):
+            A = np.triu(np.random.normal(size=(N, N)))
+            x = np.random.normal(size=(N,))
+            b = A @ x
+            x2 = regression.solve_triangular(A, b)
+            assert_allclose(x, x2, atol=1e-5)
+
+    def test_solve_rect_triangular(self):
+        for N in range(1, 20):
             for N2 in [1, 5, 100]:
                 A = np.triu(np.random.normal(size=(N + N2, N)))
                 x = np.random.normal(size=(N,))
                 b = A @ x
                 x2 = regression.solve_triangular(A, b)
                 assert_allclose(x, x2, atol=1e-5)
+
+    def test_reflection(self):
+        x = np.array([1, 1, 1])
+        e1 = np.array([1, 0, 0])
+        H = regression.householder_reflection(x, e1)
+        assert_allclose(H @ (np.sqrt(3) * np.array([1, 0, 0])), x, atol=1e-5)
+        assert_allclose(H @ np.array([1, 1, 1]), np.sqrt(3) * e1, atol=1e-5)
